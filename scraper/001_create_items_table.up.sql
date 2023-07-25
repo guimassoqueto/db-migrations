fafce3fa495d5fd04c13f8de3e3e6b83c1e1c51b
guimassoqueto <gui.massoqueto@gmail.com>
2023-07-25 10:50:12 -0300

    feat: update table migration

--- scraper/001_create_items_table.up.sql
+++ scraper/001_create_items_table.up.sql
@@ -1,13 +1,10 @@
-CREATE TABLE go_rabbit_pg (
+CREATE TABLE items (
 	"id" TEXT PRIMARY KEY NOT NULL,
 	"title" TEXT NOT NULL,
 	"category" VARCHAR(255) NOT NULL,
 	"reviews" INTEGER NOT NULL,
 	"free_shipping" BOOLEAN NOT NULL,
 	"image_url" TEXT NOT NULL,
-	"discount" INTEGER NOT NULL,
-	"price" NUMERIC(9,2) NOT NULL,
-	"previous_price" NUMERIC(9,2) NOT NULL,
 	"created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
 	"updated_at" TIMESTAMP WITH TIME ZONE NOT NULL
 );
