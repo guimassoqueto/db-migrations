ffcd085e85b659277657907e193f79020a921692
guimassoqueto <gui.massoqueto@gmail.com>
2023-09-13 09:54:07 -0300

    feat: update sneakers

--- sneakers/002_create_items_table.up.sql
+++ sneakers/002_create_items_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE items (
 	"id" VARCHAR(8) UNIQUE NOT NULL DEFAULT SPLIT_PART(cast(uuid_generate_v4() as text), '-', 1),
 	"url" TEXT PRIMARY KEY NOT NULL,
-	"afiliate_url" TEXT PRIMARY KEY NOT NULL,
+	"afiliate_url" TEXT,
 	"title" TEXT NOT NULL,
 	"category" VARCHAR(255) NOT NULL,
 	"image_url" TEXT NOT NULL,
