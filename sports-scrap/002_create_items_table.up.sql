d42eb15c7f13d971cf6fca67ed1dbfda1ec01ddb
guimassoqueto <gui.massoqueto@gmail.com>
2023-09-16 07:38:40 -0300

    feat: create sports-scrap project

---
+++ sports-scrap/002_create_items_table.up.sql
@@ -0,0 +1,15 @@
+CREATE TABLE items (
+	"id" VARCHAR(8) UNIQUE NOT NULL DEFAULT SPLIT_PART(cast(uuid_generate_v4() as text), '-', 1),
+	"url" TEXT PRIMARY KEY NOT NULL,
+	"afiliate_url" TEXT,
+	"title" TEXT NOT NULL,
+	"category" VARCHAR(255) NOT NULL,
+	"reviews" INTEGER NOT NULL,
+	"free_shipping" BOOLEAN NOT NULL,
+	"image_url" TEXT NOT NULL,
+	"price" NUMERIC(9,2) NOT NULL,
+	"previous_price" NUMERIC(9,2) NOT NULL,
+	"discount" INTEGER NOT NULL,
+	"created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+	"updated_at" TIMESTAMP WITH TIME ZONE NOT NULL
+);
